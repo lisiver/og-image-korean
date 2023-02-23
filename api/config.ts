2158289f6329ddc2d4949d4eb3feac6119e90488
lisiver <developer.lisiver@gmail.com>
2023-02-23 11:26:41 +0900

    style: dark일 경우 이미지 경로를 수정합니다

--- api/config.ts
+++ api/config.ts
@@ -1,6 +1,6 @@
 export default {
   defaultImage: {
     light: 'https://sikiy-log.vercel.app/avatar.svg',
-    dark: 'https://sikiy-log.vercel.app/avatar.svg'
+    dark: 'https://sikiy-log.vercel.app/avatar-dark.svg'
   }
 }
